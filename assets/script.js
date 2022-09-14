b3aa04cf53742c5950ed973f250480ecbb7e7731
CristianoAliotta <cristiano.aliotta@outlook.com>
2022-09-14 17:07:38 +0200

    few js experiments

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+let annoNascita = Number(prompt("Inserisci il tuo anno di nascita"));
+let price;
+let x = calcoloEta(2022, annoNascita);
+function calcoloEta(a, b) {
+  return a - b;
+}
+console.log(x);
+let prezzoBiglietto = prompt("In che stagione intendi visitare il parco?");
+if (prezzoBiglietto == "estate" || prezzoBiglietto == "primavera") {
+  price = 35;
+} else {
+  price = 20;
+}
+if (x < 10) {
+  price = 0;
+} else if ((x > 10 && x < 18) || x >= 65) {
+  price = price - (price * 20) / 100;
+}
+console.log(price);
